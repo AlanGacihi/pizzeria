fe1cbdf7896b0fae64764a75e83a9cfc5ea14b1e
AlanGacihi <alanmamicha@gmail.com>
2024-12-07 14:52:58 +0300

    fix: fresh database always

--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Make sure this file has executable permissions, run `chmod +x run-app.sh`
-# Run migrations, seed the database, process the Nginx configuration template and start Nginx
-php artisan migrate --force && php artisan db:seed --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf  /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
+# Fresh migrate the database, seed it, process the Nginx configuration template and start Nginx
+php artisan migrate:fresh --force && php artisan db:seed --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf  /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
